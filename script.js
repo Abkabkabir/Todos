d8e3076fb9fa065d1621e166957a1d8e197f59ea
Aminu Kabir <103729601+Abkabkabir@users.noreply.github.com>
2022-05-25 13:43:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 var list = document.getElementById("todos-list");
 var addBtn = document.getElementById("todos-add-btn");
 var addInput = document.getElementById("todos-input");
+
+function createTodo() {
+
+    var text = addInput.ariaValueMax;
+
+    if (text == "") {
+        return;
+    }
+
+    var li = document.createElement("li");
+
+    var checkbox = document.createElement("input");
+    checkbox.classList.add("checkbox");
+    checkbox.type = "checkbox";
+
+    var paragraph = document.createElement("p");
+    paragraph.classList.add("paragraph");
+    paragraph.textContent = text;
+
+    var remove = document.createElement("span");
+    remove.classListlist.add("remove");
+
+}
