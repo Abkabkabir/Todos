779c4b8484015bd5388532cd860c662099a592af
Aminu Kabir <103729601+Abkabkabir@users.noreply.github.com>
2022-05-25 17:47:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -22,5 +22,71 @@ function createTodo() {
 
     var remove = document.createElement("span");
     remove.classListlist.add("remove");
+    remove.innerHTML = "&cross";
+
+    li.appendChild(checkbox);
+    li.appendChild(checkbox);
+    li.appendChild(checkbox);
+    list.appendChild(li);
+
+    addInput.value = "";
 
 }
+
+function showEditInput(paragraphElement) {
+
+    var editInput = document.getElementsByName("editInput")[0];
+    if (editInput) {
+        editInput.remove();
+    }
+
+    var input = document.creatElement("input");
+    input.type = "text";
+    input.name = "editInput";
+    input.value = paragraphElement.textContent;
+    input.classList.add("editInput");
+
+    paragraphElement.parentElement.appendChild(input);
+    input.focus();
+
+}
+
+function removeTodo(removeElement) {
+    removeElement.parentElement.remove();
+}
+
+function toggleComplete(inputElement) {
+    if (inputElement.checked === false) {
+        inputElement.parentElement.classList.remove("remove");
+
+    } else {
+        inputElement.parentElement.classList, remove("complete");
+    }
+}
+
+list.addEventListener("click", function(event) {
+
+    switch (event.target.tagName) {
+        case "p":
+            showEditInput();
+            break;
+        case "SPAN":
+            removeTodo();
+            break;
+    }
+});
+
+list.addEventListener("change", function(event) {
+    if (event.target.tagName === "INPUT" && event.target.type ===
+        "checkbox") {
+        toggleComplete(event.target);
+    }
+});
+
+addBtn.addEventListener("click", createTodo);
+
+addInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        createTodo();
+    }
+});
